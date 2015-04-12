51b02ed5349a13069685bb317fdf8e1b647d5461
nagaic <bomsap700@gmail.com>
2015-04-12 16:14:23 +0900

    add jade views

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.render('index', { title: 'Express Sample' });
 });
 
 app.listen(app.get('port'), function() {

c0de7d849ecacea33b537b9c1fc17a960b00a5cf
nagaic <bomsap700@gmail.com>
2015-04-15 23:37:36 +0900

    implement reload button and reload 3 imgs.

--- public/js/get_json.js
+++ public/js/get_json.js
@@ -1,11 +1,13 @@
 $(function(){
   $('[data-action=reload]').on('click', function() {
-    $.getJSON("http://www.lgtm.in/g", function (data) {
-      $("textarea").
-        append("\n\n" + data.markdown);
-        console.log(data);
-      $("body").
-        append("<img src=" + data.imageUrl + ">");
+    $(".lgtm_img").each(function(i, this_obj){
+      $.getJSON("http://www.lgtm.in/g", function (data) {
+        $("textarea")
+          .append("\n\n" + data.markdown);
+        $(this_obj)
+          .attr('src', data.imageUrl)
+          .attr('copy', '![LGTM](' + data.imageUrl + ')');
+      });
     });
   });
 });

f9c4420d8c8624e2de5fbce270c5d88b14508ee1
nagaic <bomsap700@gmail.com>
2015-04-18 19:45:19 +0900

    implement grunt concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,37 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
+
       pkg: grunt.file.readJSON('package.json'),
 
-      // concat: {
-      //     files: {
-      //         // 元ファイルの指定。
-      //         src : 'public/js/src/*.js',
-      //         // 出力ファイルの名前・パス指定
-      //         dest: 'public/js/logic.js'
-      //     }
-      // },
+      concat: {
+          files: {
+              // 元ファイルの指定。
+              src : 'public/js/src/*.js',
+              // 出力ファイルの名前・パス指定
+              dest: 'public/js/src/client.js'
+          }
+      },
       uglify: {
-          // dist: {
-          //     files: {
-          //         // 出力ファイル: 元ファイル
-          //         'public/js/*.min.js': 'public/js/*.js'
-          //     }
-          // }
-          my_target: {
-              files: [{
-                  expand: true,
-                  cwd: 'public/js/src',
-                  src: '*.js',
-                  dest: 'public/js/',
-                  ext: '.min.js'
-              }]
+          dist: {
+              files: {
+                  // 出力ファイル: 元ファイル
+                  'public/js/client.min.js': 'js/src/client.js'
+              }
           }
       },
       watch: {
           js: {
               files: 'public/js/src/*.js', // 監視対象ファイル
-//              tasks: ['concat', 'uglify'] // 実行させるタスク
-              tasks: ['uglify'] // 実行させるタスク
+              tasks: ['concat', 'uglify'] // 実行させるタスク
           }
       }
   });
 
-  //grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  //grunt.registerTaks('default', ['concat']);
-  grunt.registerTask('default', ['uglify']);
-
-  };
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
 

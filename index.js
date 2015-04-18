840dee2d575ec00f8237e017627e72a37746291a
nagaic <bomsap700@gmail.com>
2015-04-19 01:16:09 +0900

    install socket.io

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
+// server configure
+app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+app.use(express.static(__dirname + '/public'));
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+}
+
+
+// server listen
+server.listen(app.get('port'), function () {
+  console.log('Server listening at port %d', app.get('port'));
+});
+
+
+// route
 app.get('/', function(request, response) {
   response.render('index', { title: 'Express Sample' });
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+
+// Socket.io
+var socket_io = require('socket.io');
+var io = socket_io.listen(server);
+
+var recommend = {};
+
+io.on('connection', function (socket) {
+
+  socket.on('select image', function (data) {
+    console.log(data.img);
+    socket.broadcast.emit('add recommend', {
+      img: data.img,
+    });
+  });
 });

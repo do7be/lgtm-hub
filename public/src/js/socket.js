f24776c8e37c5a7c526f0da40f931956d67b3a02
nagaic <bomsap700@gmail.com>
2015-05-09 18:56:44 +0900

    modify css for recommend images

--- public/src/js/socket.js
+++ public/src/js/socket.js
@@ -7,7 +7,17 @@ $(function(){
   });
 
   socket.on('add recommend', function (data) {
-    $('#recommend_img_area').append('<img src="' + data.img + '">');
+    var image_url = '![LGTM](' + data.imageUrl + ')';
+
+    var img_box = $('<div class="recommend_img_box"></div>')
+      .append('<img src="' + data.img + '">');
+
+    var recommend_box = $('<div class="text-center"></div>')
+      .append(img_box)
+      .append('<buttton data-clipboard-text="' + image_url + '" class="lgtm_img_copy btn btn-warning btn-small" data-toggle="tooltip" data-placement="bottom" title="Copied">Copy</button>');
+
+    $('#recommend_img_area')
+      .append(recommend_box);
   });
 });
 

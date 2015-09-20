cf727cabc48a138f3f0928775b1dd6ddac0b23a6
do7be <bomsap700@gmail.com>
2015-09-20 21:51:01 +0900

    add gulp settings for Node restart and brower reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-var gulp    = require("gulp"),
-    uglify  = require("gulp-uglify"),
-    concat  = require("gulp-concat"),
-    rename  = require('gulp-rename'),
-    plumber = require("gulp-plumber");
+var gulp       = require("gulp"),
+    uglify     = require("gulp-uglify"),
+    concat     = require("gulp-concat"),
+    rename     = require('gulp-rename'),
+    plumber    = require("gulp-plumber"),
+    spawn      = require('child_process').spawn,
+    livereload = require('gulp-livereload'),
+    server;
 
 // concat & uglify
 gulp.task('js.concat', function() {
@@ -22,9 +25,44 @@ gulp.task('js.uglify', function() {
 // js build
 gulp.task('js', ['js.concat', 'js.uglify']);
 
+// server with node
+gulp.task('server',function(){
+  if(server){
+    server.kill('SIGKILL');
+    server=undefined;
+  }
+
+  // start server
+  server = spawn('node', ['index']);
+
+  // console output
+  server.stdout.setEncoding('utf8');
+  server.stdout.on('data', function(data){
+    console.log(data);
+  });
+  // console output with error
+  server.stderr.setEncoding('utf8');
+  server.stderr.on('data', function(data){
+    console.log(data);
+  });
+})
+
+// reload browser
+gulp.task('reload', function() {
+  gulp.src(['public/*/*', 'views/*'])
+    .pipe(livereload());
+})
+
 // watch
-gulp.task('watch', function() {
+gulp.task('watch',['server'],function(){
+  livereload.listen();
+
+  // watch for compile
   gulp.watch(['public/src/js/*.js'], ['js']);
-});
+  // watch for server restart
+  gulp.watch(['index.js'], ['server']);
+  // watch for browser reload
+  gulp.watch(['index.js', 'public/*/*', 'views/*'], ['reload']);
+})
 
 gulp.task('default', ['js']);

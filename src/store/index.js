754f80e5612788d3fea7649a935dfd67242ed225
do7be <bomsap700@gmail.com>
2018-03-11 17:47:54 +0900

    ProductionではReduxDevToolsを無効にした

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { createStore } from 'redux'
 import rootReducer from '../reducers'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
+
+const composeEnhancers = composeWithDevTools({})
 
 const rootStore = () => {
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 
   return store

151c36453bdb3477737d0d687762fa8a867f00b2
do7be <bomsap700@gmail.com>
2018-03-12 19:24:34 +0900

    webpack4の形式でmodeによるproduction判定を行えるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,48 @@ const webpack = require('webpack')
 const ManifestPlugin = require('webpack-manifest-plugin')
 const CleanAssetsPlugin = require('clean-assets-webpack-plugin')
 
-module.exports = {
-  // modeをわたすようにしたい
-  mode: 'production',
-  entry: {
-    'index': [
-      path.resolve(__dirname, 'src/index.jsx')
-    ]
-  },
-  output: {
-    filename: process.env.NODE_ENV !== 'production'
-      ? '[name].dev.js'
-      : '[name].[hash].js',
-    path: path.resolve(__dirname, 'public/js/dist'),
-    publicPath: '/',
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new ManifestPlugin({ fileName: path.resolve(__dirname, 'manifest.json') }),
-    new CleanAssetsPlugin()
-  ],
-  optimization: {
-    minimize: process.env.NODE_ENV === 'production',
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      'index': [
+        path.resolve(__dirname, 'src/index.jsx')
+      ]
+    },
+    output: {
+      filename: argv.mode !== 'production'
+        ? '[name].dev.js'
+        : '[name].[hash].js',
+      path: path.resolve(__dirname, 'public/js/dist'),
+      publicPath: '/',
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        '__DEV__': argv.mode !== 'production'
+      }),
+      new ManifestPlugin({ fileName: path.resolve(__dirname, 'manifest.json') }),
+      new CleanAssetsPlugin()
+    ],
+    optimization: {
+      minimize: argv.mode === 'production',
+      splitChunks: {
+        chunks: 'all'
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  }
 };

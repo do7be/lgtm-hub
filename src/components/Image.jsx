2aa30a6d580a569ab088cd6613c8b6a3e43e03f1
do7be <bomsap700@gmail.com>
2018-03-17 18:54:32 +0900

    デザインリニューアル: サムネイル部分を実装

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames'
 
-import { Tooltip } from 'react-tippy'
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -9,10 +8,12 @@ import { selectImage } from '../actions'
 
 import * as style from './Image.scss'
 
+import Check from './check.svg'
+
 class Image extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { openTooltip: false }
+    this.state = { copyAnimation: false }
     this.onClickCopy = this.onClickCopy.bind(this)
     this.refToClipBoard = this.refToClipBoard.bind(this)
   }
@@ -20,29 +21,21 @@ class Image extends React.Component {
   render () {
     const { small } = this.props
     return (
-      <div>
-        <div className={classNames(style.imgBox, small && style.small)}>
-          <img className={style.lgtmImg} src={this.props.url}/>
-        </div>
-        {/* buttonはコンポーネント化したい */}
-        <Tooltip
-          title='Copied'
-          position='bottom'
-          trigger='manual'
-          open={this.state.openTooltip}
-          hideDelay={100}
-          arrow
+      <div className={classNames(style.container, small && style.small)}>
+        <button
+          type='button'
+          onClick={this.onClickCopy}
+          data-clipboard-text={this.props.clip_board}
+          className={classNames(style.copyButton, this.state.copyAnimation && style.copied)}
+          style={{ backgroundImage: `url('${this.props.url}')`}}
+          ref={this.refToClipBoard}
         >
-          <button
-            type='button'
-            onClick={this.onClickCopy}
-            data-clipboard-text={this.props.clip_board}
-            className={classNames(!small && style.lgtmImgCopy, 'btn', 'btn-warning', small ? 'btn-small' : 'btn-large')}
-            ref={this.refToClipBoard}
-          >
-            Copy
-          </button>
-        </Tooltip>
+          <div className={style.border}/>
+          <div className={style.copiedNotice}>
+            <Check width={72} height={72} className={style.check}/>
+            <span className={style.text}>Copied</span>
+          </div>
+        </button>
       </div>
     )
   }
@@ -57,10 +50,10 @@ class Image extends React.Component {
   onClickCopy () {
     this.props.actions.selectImage({ img: this.props.url })
 
-    this.setState({ openTooltip: true }, () => {
+    this.setState({ copyAnimation: true }, () => {
       setTimeout(() => {
-        this.setState({ openTooltip: false })
-      }, 1000)
+        this.setState({ copyAnimation: false })
+      }, 1600)
     })
   }
 }
